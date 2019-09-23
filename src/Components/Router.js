c811aa5f1be02df11114afeff61aaaf4d6b369c0
hyung <hj.gwon@specupad.com>
2019-09-23 11:40:20 +0900

    Add CSS in React
    - GlobalStyles
    - Header
    - Location Aware Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
-    <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
-    </Router>
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
 );
