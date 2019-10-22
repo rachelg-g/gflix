8b57ff47f740b0a8c40911e2ecad82b178e17c2d
hyung <hj.gwon@specupad.com>
2019-10-22 18:04:32 +0900

    Updates

---
+++ precache-manifest.b05086c4df40004ea89c7356366e30ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cf2b0b02c1332a2868668b061f1627",
+    "url": "/Nomflix/index.html"
+  },
+  {
+    "revision": "05c3854534cbe50fa0a8",
+    "url": "/Nomflix/static/js/2.dd5e46f1.chunk.js"
+  },
+  {
+    "revision": "312c14ed55927d885bec",
+    "url": "/Nomflix/static/js/main.231adc99.chunk.js"
+  },
+  {
+    "revision": "13ae5b517436184f7f68",
+    "url": "/Nomflix/static/js/runtime~main.aec02646.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/Nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix/precache-manifest.19024e0effd889f18dafa661f05a1dfb.js"
+  "/Nomflix/precache-manifest.b05086c4df40004ea89c7356366e30ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nomflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
