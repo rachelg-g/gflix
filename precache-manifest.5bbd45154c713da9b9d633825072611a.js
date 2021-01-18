67b36546ddaca494d69fef54afdc11739b510bf2
rachelg-g <hj.gwon@specupad.com>
2021-01-18 16:42:50 +0900

    Updates

---
+++ precache-manifest.5bbd45154c713da9b9d633825072611a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb842626dc99ca0c8f3e97797561c398",
+    "url": "/gflix/index.html"
+  },
+  {
+    "revision": "776f478a317646587bc5",
+    "url": "/gflix/static/js/2.c5e53365.chunk.js"
+  },
+  {
+    "revision": "7f3aa0708872473d0359",
+    "url": "/gflix/static/js/main.6e01446a.chunk.js"
+  },
+  {
+    "revision": "b98cd8a319683f6a1ffb",
+    "url": "/gflix/static/js/runtime~main.3d03a522.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/gflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
